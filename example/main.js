e8edcf0fb3ffe787c01fcaa328977d06a5d33cf6
ducmanh02 <89408872+ducmanh02@users.noreply.github.com>
2022-11-08 09:05:08 +0700

    a

--- example/main.js
+++ example/main.js
@@ -2,3 +2,8 @@ var username = document.querySelector('#username')
 var email = document.querySelector('#email')
 var password = document.querySelector('#password')
 var confirmPassword = document.querySelector('#confirm-password')
+
+function showError(input){
+    input.value = input.value.trim();
+    
+}
